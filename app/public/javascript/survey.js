d760da9dadd7eb72326ce633481fa86f4278c0b6
Edwin <99057140+EdwinRodriguezcode@users.noreply.github.com>
2022-03-10 11:42:01 -0800

    Update survey.js

---
+++ app/public/javascript/survey.js
@@ -0,0 +1,18 @@
+var survey = [];
+  var question = [
+  'Around how many frends and family are invited ?'
+  'What kind of setting would you like ?'
+  'what kind of flowers you like ?'
+  'Do you want the cerimony out doors ?'
+  'Do you liken live music ?'
+  'Do you want to have a cerimony in a church ?'
+  'Do you like any particulare kind of theme ?'
+  'Do you want to have a reception after then cerimony ?'
+  'Would you like to make it a weekend event ?'
+  'owuld you like us to provide a profesional photographer ?'
+  ]
+  
+  for (var idx = 0; idx < 10; idx++) {
+   var newQuestion = $(`<div class='questionBar' id='question${idx}'><h6>${questions[idx]}</h6></div>`);
+ })
+  
